641f9d9fd600ed133b9be82f31b0e432918a2e65
Soraya Rezaie <banorezaie2004@gmail.com>
2024-02-29 11:54:44 +0430

    calculator

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
 num1 = int(input("Give me the first number "))
 num2 =int(input("Give me the second number"))
-operand = input("give me the operand ")
-num3 = num1 + num2
-print(num3)
+operand = input("give me the operator")
+if operand == '+':
+    result = num1 + num2
+elif operand == '-':
+    result = num1 - num2
+elif operand == '*':
+    result = num1 * num2
+elif operand == '/':
+    if num1 != 0:
+        result = num1 / num2
+    else:
+        print('It cannot be divided by zero')
+else:
+    print('Invalid Operator')
+print(result)
